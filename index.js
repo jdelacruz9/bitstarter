603761c40191346942f88d685404d49d3db3a679
Julio de la Cruz <jjdl_cn@hotmail.com>
2014-11-01 18:04:54 -0400

    arreglando el index

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html", function(buf){
-    return buf.toString()
-  })
-})
+    buf.toString()
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
