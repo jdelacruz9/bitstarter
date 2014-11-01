94e9a6d19bb76fd5d74e54477a8d7844895f0583
jdelacruz9 <jjcnatera@gmail.com>
2014-11-01 20:46:25 +0000

    tratando

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express')
-var app = express.createServer(express.logger());
-app.get('/', function(request, response) {
-  response.send('Hello World2!')
-});
+var app = express();
 
-var port = process.env.PORT || 5000; 
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
